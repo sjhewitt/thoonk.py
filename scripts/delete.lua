4b632e58108d4df412acd167e747bdc21ca61ddd
Simon Hewitt <si@sjhewitt.co.uk>
2011-11-20 16:34:54 +0000

    delete all feed keys when deleting feed

--- scripts/delete.lua
+++ scripts/delete.lua
@@ -2,8 +2,39 @@
 if redis.call('srem', 'feeds', ARGV[1]) == 0 then
     return false
 end
+schema = {
+  feed = function(name) return {
+    'feed.config:'..name,
+    'feed.ids:'..name,
+    'feed.items:'..name,
+    'feed.publishes:'..name
+  } end,
+  sortedfeed = function(name) return {
+    'feed.config:'..name,
+    'feed.ids:'..name,
+    'feed.items:'..name,
+    'feed.publishes:'..name,
+    'feed.idincr:'..name
+  } end,
+  queue = function(name) return {
+    'feed.config:'..name,
+    'feed.ids:'..name,
+    'feed.items:'..name,
+    'feed.publishes:'..name
+  } end,
+  job = function(name) return {
+    'feed.config:'..name,
+    'feed.ids:'..name,
+    'feed.items:'..name,
+    'feed.publishes:'..name,
+    'feed.published:'..name,
+    'feed.claimed:'..name,
+    'feed.cancelled:'..name,
+    'feed.finishes:'..name,
+    'feed.stalled:'..name
+  } end
+}
 feedtype = redis.call('hget', 'feed.config:'..ARGV[1], 'type')
--- TODO: delete schema keys!
-redis.call('del', 'feed.config:'..ARGV[1])
+redis.call('del', unpack(schema[feedtype](ARGV[1])))
 redis.call('publish', 'delfeed', ARGV[1]..'\0'..ARGV[2])
 return true
